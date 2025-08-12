563ef21653cc0aa588ab0c0dab557d82ea847498
William <vimscientist69@gmail.com>
2025-08-12 02:03:14 -0600

    refactor(HeliumIntegrationTests): all browser start methods in `StartBrowsersIntegrationTests`, `goTo` method tests in `GoToMethodIntegrationTests`

--- Tests/HeliumSwiftTests/GoToMethodIntegrationTests.swift
+++ Tests/HeliumSwiftTests/GoToMethodIntegrationTests.swift
@@ -1,4 +1,4 @@
-// HeliumIntegrationTests.swift
+// GoToMethodIntegrationTests.swift
 // Copyright (c) 2025 GetAutomaApp
 // All source code and related assets are the property of GetAutomaApp.
 // All rights reserved.
@@ -7,15 +7,8 @@ import Testing
 
 @testable import HeliumSwift
 
-@Suite("`Helium` Integration Tests")
-internal struct HeliumIntegrationTests {
-    /// Test start chrome driver instance
-    @Test("Test `startChrome` method")
-    public func startChromeMethod() async throws {
-        let driver = try await Helium.startChrome()
-        try await driver.stop()
-    }
-
+@Suite("`goTo Method Integration Tests")
+internal struct GoToMethodIntegrationTests {
     /// Test `goTo` method success - valid urls
     @Test(
         "Test `goTo` method success cases",
---
+++ Tests/HeliumSwiftTests/StartBrowsersIntegrationTests.swift
@@ -0,0 +1,18 @@
+// StartBrowsersIntegrationTests.swift
+// Copyright (c) 2025 GetAutomaApp
+// All source code and related assets are the property of GetAutomaApp.
+// All rights reserved.
+
+import Testing
+
+@testable import HeliumSwift
+
+@Suite("Start Browsers Integration Tests")
+internal struct StartBrowsersIntegrationTests {
+    /// Test start chrome driver instance
+    @Test("Test `startChrome` method")
+    public func startChromeMethod() async throws {
+        let driver = try await Helium.startChrome()
+        try await driver.stop()
+    }
+}
