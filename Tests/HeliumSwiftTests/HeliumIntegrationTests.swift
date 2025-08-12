c940f74f776cee52285b2556897791c7d55f2a72
William <vimscientist69@gmail.com>
2025-08-12 01:13:00 -0600

    test(HeliumIntegrationTests): wrote tests for `Helium.goTo()` method, for success and failure cases
    Tests passed as expected. Success case: valid URLs. Failure case: invalid urls (no scheme)
    
    Additional tasks: create a failure case for urls with invalid schemes

--- Tests/HeliumSwiftTests/HeliumIntegrationTests.swift
+++ Tests/HeliumSwiftTests/HeliumIntegrationTests.swift
@@ -15,4 +15,26 @@ internal struct HeliumIntegrationTests {
         let driver = try await Helium.startChrome()
         try await driver.stop()
     }
+
+    /// Test `goTo` method success - valid urls
+    @Test(
+        "Test `goTo` method success cases",
+        arguments: ["https://google.com", "https://youtube.com", "https://instagram.com"]
+    )
+    public func goToMethodValidURLs(url: String) async throws {
+        let driver = try await Helium.startChrome()
+        try await Helium.goTo(driver: driver, urlString: url)
+    }
+
+    /// Test `goTo` method failure cases - invalid urls
+    @Test(
+        "Test `goTo` method failure cases - invalid urls",
+        arguments: ["anotherInvalidURL", "invalidURL", "httpsinvalid-url"]
+    )
+    public func goToMethodInvalidURLs(url: String) async throws {
+        let driver = try await Helium.startChrome()
+        await #expect(throws: HeliumError.invalidURL(url: url, reason: "No scheme"), "Invalid URL") {
+            try await Helium.goTo(driver: driver, urlString: url)
+        }
+    }
 }
