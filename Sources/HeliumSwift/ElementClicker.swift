efb7f55ed9be6a10f2596a11bea4359341cfe936
William <vimscientist69@gmail.com>
2025-08-13 12:21:43 -0600

    docs(ElementClicker): documented code

--- Sources/HeliumSwift/ElementClicker.swift
+++ Sources/HeliumSwift/ElementClicker.swift
@@ -8,10 +8,14 @@ import SwiftWebDriver
 internal struct ElementClicker {
     private let element: Element
 
+    /// Initialize `ElementClicker`
+    /// - Parameter element: `Element`, the SwiftWebDriver element you want to click on
     public init(element: Element) {
         self.element = element
     }
 
+    /// Click on the element
+    /// - Throws: An error if there is a problem clicking on the element
     public func click() async throws {
         try await element.click()
     }
