49f7690269176853d2d25ff6586d128257d2b08e
William Ferns <vimscientist69@gmail.com>
2026-07-07 18:56:04 +0200

    Helium Press Method Implemented (#17)
    * feat(Helium): create press method, with integration tests, working as expected
    
    * docs(Helium): fixed methods doc strings and added docs strings for new press methods

--- Sources/HeliumSwift/Helium.swift
+++ Sources/HeliumSwift/Helium.swift
@@ -1,5 +1,5 @@
 // Helium.swift
-// Copyright (c) 2025 GetAutomaApp
+// Copyright (c) 2026 GetAutomaApp
 // All source code and related assets are the property of GetAutomaApp.
 // All rights reserved.
 
@@ -17,67 +17,101 @@ public enum Helium {
     public static func startChrome(payload: ChromeStarterPayload? = nil)
         async throws -> WebDriver<ChromeDriver>
     {
-        return try await ChromeStarter(payload: payload).startChrome()
+        try await ChromeStarter(payload: payload).startChrome()
     }
 
     /// Send text to a `AnyElement`
     /// - Parameters:
-    ///   - text: Text to send to element
-    ///   - element: `E`, find an element from the driver
+    ///   - text: `String`, text to send to element
+    ///   - element: `<some AnyElement>`, find an element from the driver
     ///
     /// - Throws: An error if there is a problem sending keys to the element
-    public static func write<E: AnyElement>(text: String, element: E) async throws {
+    public static func write(text: String, element: some AnyElement) async throws {
         try await ElementWriter(text: text, element: element.underlyingElement).write()
     }
 
     /// Send text to the active element in the current window of the passed in driver
     /// - Parameters:
     ///   - text: `String`, text to send to element
-    ///   - driver: `WebDriver<T>`, the driver to send the keys to
+    ///   - driver: `<some Driver>`, the driver to send the keys to
     ///
     /// - Throws: An error if there is a problem sending keys to the currently active element
-    public static func write<T: Driver>(text: String, driver: WebDriver<T>) async throws {
+    public static func write(text: String, driver: WebDriver<some Driver>) async throws {
         try await write(text: text, element: driver.getActiveElement())
     }
 
     /// Opens the specified URL in the passed in web driver window
     /// - Parameters:
-    ///   - driver: `WebDriver<T>`, The driver you want the current window to navigate to the passed in URL
+    ///   - driver: `WebDriver<some Driver>`, The driver you want the current window to navigate to the passed in URL
     ///   - urlString: `String`, The URL you want to navigate to you wan
     ///
     /// - Throws: `Helium.invalidURL` when URL format is invalid or an error when driver navigation failed
-    public static func goTo<T>(driver: WebDriver<T>, urlString: String) async throws {
+    public static func goTo(driver: WebDriver<some Driver>, urlString: String) async throws {
         let url = try URL.fromString(payload: .init(string: urlString, asHttp: true))
         try await driver.navigateTo(url: url)
     }
 
     /// Click on a `AnyElement`
-    /// - Parameter element: `E`, the element to click on
+    /// - Parameter element: `some AnyElement`, the element to click on
     /// - Throws: An error if there is a problem clicking on the element
-    public static func click<E: AnyElement>(element: E) async throws {
+    public static func click(element: some AnyElement) async throws {
         try await ElementClicker(element: element.underlyingElement).click()
     }
 
     /// Double click on a `AnyElement`
     /// initializers
-    /// - Parameter element: `E`, the element to click on
+    /// - Parameter element: `some AnyElement`, the element to click on
     /// - Throws: An error if there is a problem clicking on the element
-    public static func doubleClick<E: AnyElement>(element: E) async throws {
+    public static func doubleClick(element: some AnyElement) async throws {
         try await element.underlyingElement.doubleClick()
     }
 
     /// Drag and drop an element to another element
     /// - Parameters:
-    ///   - driver: `D`, The driver you want to use to perform drag and drop implementation
-    ///   - element: `E1`, the source element you want to drag
-    ///   - targetElement: `E2`, the target element you want to drag the source element to
+    ///   - driver: `WebDriver<some Driver>`, The driver you want to use to perform drag and drop implementation
+    ///   - element: `some AnyElement`, the source element you want to drag
+    ///   - targetElement: `some AnyElement`, the target element you want to drag the source element to
     ///
     /// - Throws: An error if there is a problem dragging the source element to the target element
-    public static func drag<E1: AnyElement, E2: AnyElement, T, D: WebDriver<T>>(
-        driver: D,
-        element: E1,
-        to targetElement: E2
+    public static func drag(
+        driver: WebDriver<some Driver>,
+        element: some AnyElement,
+        to targetElement: some AnyElement
     ) async throws {
         try await driver.dragAndDrop(from: element.underlyingElement, to: targetElement.underlyingElement)
+    }
+
+    /// Press keys and characters in combination to the currently active element
+    /// - Parameters:
+    ///   - keys: `[ElementTypes.SendValueActionKeyTypes?]`, the keys to chord to the active element, can be left as an
+    /// empty array
+    ///   - characters: `String`, characters to send to the current active element, can be left as empty or a sequence
+    /// of elements provided
+    ///   - driver: `WebDriver<some Driver>`, The driver to send or chord keys and characters to
+    ///
+    /// - Throws: An error if there is a problem sending or chording keys and characters to the currently active element
+    public static func press(
+        _ keys: [ElementsTypes.SendValueActionKeyTypes?],
+        _ characters: String = "",
+        _ driver: some WebDriver<some Any>,
+    ) async throws -> String? {
+        try await driver.getActiveElement().sendKeys(keys: keys, characters: characters)
+    }
+
+    /// Press keys and characters in combination to a `AnyElement`
+    /// - Parameters:
+    ///   - keys: `[ElementTypes.SendValueActionKeyTypes?]`, the keys to chord to the active element, can be left as an
+    /// empty array
+    ///   - characters: `String`, characters to send to the current active element, can be left as empty or a sequence
+    /// of elements provided
+    ///   - element: `some AnyElement`, find an element from the driver
+    ///
+    /// - Throws: An error if there is a problem sending or chording keys and characters to the currently active element
+    public static func press(
+        _ keys: [ElementsTypes.SendValueActionKeyTypes?],
+        _ characters: String = "",
+        _ element: some AnyElement
+    ) async throws -> String? {
+        try await element.underlyingElement.sendKeys(keys: keys, characters: characters)
     }
 }
